7e418248b5ae0580adfdc18af10274695d103746
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-11 22:42:38 +0300

    diziyib

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 3
+#define M 2
+void tekdenCifte(int c[][M],int d[],int e);
 int main()
 {
-    int a[3][3],i,j;
-    printf("Dizinin Elemanlari\n");
-    for(i=0;i<3;i++)
+    int d[6]={1,2,3,4,5,6};
+    int c[N][M]={0};
+    tekdenCifte(c,d,6);
+    return 0;
+}
+
+void tekdenCifte(int c[][M],int d[],int e)
+{
+    int i,j,a=0;
+    for(i=0;i<N;i++)
     {
-        printf("\na dizisinin %d.satiri:",i+1);
-        for(j=0;j<3;j++)
-            scanf("%d",&a[i][j]);
+        for(j=0;j<M;j++)
+            c[i][j]=d[a];
+       a++;
     }
-    printf("\n-------------------------------------------------\n");
-    for(j=0;j<3;j++)
+    printf("\n");
+    for(i=0;i<N;i++)
     {
-        for(i=0;i<3;i++)
-          printf("%3d",a[i][j]);
+        for(j=0;j<M;j++)
+            printf("%d",c[i][j]);
       printf("\n");
     }
-    return 0;
 }
