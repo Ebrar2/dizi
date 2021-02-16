78be67844883fcaa1dbab60796b75ccea527e569
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 15:54:23 +0300

    2 boyutlu dizinin ust ucgensel halini tek boyutlu diziye atama

--- main.c
+++ main.c
@@ -1,49 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 4
+#define M 4
+void diziBol(int d[][M],int x);
 int main()
 {
-    char d[6][5];
-    int i,j,k=65,a[10],gec,x,b[2];
-    for(j=0;j<5;j++)
+    int d[N][M],i,j;
+    for(i=0;i<N;i++)
     {
-       for(i=0;i<6;i++)
-         {
-             if(i==4 && j==4)
-                d[i][j]='.';
-             else if(i==5 && j==4)
-                 d[i][j]='?';
-             else
-              {
-                d[i][j]=k;
-                k++;}
-
-    }}
-    for(i=0;i<6;i++)
-    {
-        printf("%d.satir:",i+1);
-          for(j=0;j<5;j++)
-             printf("%c",d[i][j]);
-      printf("\n");
+        printf("%d.satiri giriniz:",i+1);
+         for(j=0;j<M;j++)
+         scanf("%d",&d[i][j]);
+        printf("\n");
     }
-    printf("\nSifreli mesajin sayilarini giriniz:\n");
-    for(i=0;i<10;i++)
-        scanf("%d",&a[i]);
-    printf("\n--------------------------------------------\n");
-      int z=0;
-    for(i=0;i<10;i++)
-    {
-        gec=a[i];
-        z=0;
-        while(gec!=0)
-        {
-
-            x=gec%10;
-            gec=gec/10;
-            b[z]=x;
-            z++;
-        }
-    printf("%c",d[b[1]+1][b[0]+1]);
-    }
+    printf("\n------------------------------------------\n");
+    printf("Sol ucgen:");
+    diziBol(d,1);
+    printf("\n------------------------------------------\n");
+    printf("Sag ucgen:");
+    diziBol(d,0);
     return 0;
+}
+void diziBol(int d[][M],int x)
+{
+    int i,j,say=0,b=0;
+    if(x==1)
+    {
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if((i+j)<N-1)
+                        say++;
+
+
+            }
+        }
+
+        int a[say];
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if((i+j)<N-1)
+                   {a[b]=d[i][j];
+                   b++;
+            }
+        }}
+        for(i=0;i<say;i++)
+            printf("%3d",a[i]);
+    }
+   else  if(x==0)
+    {
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if((i+j)>N-1)
+                        say++;
+            }
+        }
+        int a[say];
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if((i+j)>N-1)
+                   {a[b]=d[i][j];
+                   b++;
+            }
+        }}
+        for(i=0;i<say;i++)
+            printf("%3d",a[i]);
+    }
+    else
+        printf("\nHatali x degeri!");
 }
