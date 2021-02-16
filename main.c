5e581c12ade9066e491c42e427570947ad3daa3f
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:16:41 +0300

    1001. i asal sayi

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+int asalMi(int j);
 int main()
 {
-    long int i,j,z,top;
-    for(i=150; ;i++)
+    int d[10002],i,j;
+    d[0]=2;
+    i=1;
+    for(j=3;;j++)
     {
-        for(z=100;z<i;z++)
+       if(asalMi(j)==0)
+                { d[i]=j;
+                i++;}
+        if(i==10002)
         {
-            for(j=100;j<i;j++)
-            {
-                top=i+j+z;
-                if(top==1000 && j*j+z*z==i*i)
-                {
-                    printf("\nOzel uclu carp:%ld",i*z*j);
-                    break;
-                     }
-            }
+              printf("Ara:%d",d[10002]);
+              break;
         }
     }
+
     return 0;
+}
+int asalMi(int j)
+{
+    int kont=0,i;
+    for(i=2;i<j/2;i++)
+    {
+        if(j%i==0)
+            kont++;
+    }
+    if(kont==0)
+        return 0;
+    else
+        return 1;
 }
