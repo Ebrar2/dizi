61bda44c76c8ae3f2d3445e49b94f72b97213179
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:14:08 +0300

    ucgensel sayi ve bolenleri

--- main.c
+++ main.c
@@ -3,36 +3,25 @@
 
 int main()
 {
-    int d[4][6],i,j,elde=0,top=0;
-    for(i=0;i<4;i++)
-    {
-        printf("\n%d.satir :",i);
-        for(j=0;j<6;j++)
-            scanf("%d",&d[i][j]);
-      printf("\n");
-    }
-    printf("\n------------------------------\n");
-    int a[6];
-    for(i=0;i<4;i++)
-    {
-        printf("\n%d.satir :",i);
-        for(j=0;j<6;j++)
-            printf("%3d",d[i][j]);
-      printf("\n");
-    }
-    int x;
-    for(i=0;i<6;i++)
+    int d[150000],x,i,say=0;
+    int top=0;
+    d[0]=1;
+    for(i=500;i<15000;i++)
     {
         top=0;
-       for(j=0;j<4;j++)
-           top=top+d[j][5-i];
-
-    x=top%10+elde;
-    elde=top/10;
-    a[5-i]=x;
+        for(x=1;x<=i+1;x++)
+            top=top+x;
+      d[i]=top;
     }
-    printf("\n      ");
-    for(i=0;i<6;i++)
-        printf("%3d",a[i]);
+    for(i=500;i<14000;i++)
+    {
+           say=0;
+      for(x=1;x<=d[i]/2;x++)
+          if(d[i]%x==0)
+            say++;
+    if(say>=500)
+        {printf("\nAranan(d[%d])sayi:%d sayac:%d",i,d[i],say);
+           break;
+    }}
     return 0;
 }
