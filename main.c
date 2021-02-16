40328b4ba7cec9155f59acf908338ee5b0abc35f
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:22:15 +0300

    bouncy sicrayan sayi

--- main.c
+++ main.c
@@ -1,68 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
-int asalYolla(int j);
-int asalMi(int i);
-
+int basamakSayi(int i);
 int main()
 {
-    int i,top,j,a,kont;
-    for(i=36;i<6000;i++)
+    int i,gec,rak,basamak,n,kont1,kont2,sicrayan=0;
+    for(i=1;;i++)
     {
-        kont=0;
-      if(i%2!=0 && asalMi(i)==1)
-         {
-           for(a=1;a<sqrt(i/2);a++)
-           {
-               for(j=2;j<=i-2;j++)
-               {
-                   if(asalYolla(j)==j)
-                        top=j+2*a*a;
+        kont1=0;
+        kont2=0;
+        n=0;
+        gec=i;
+        basamak=basamakSayi(i);
+        int say[basamak];
+        while(gec!=0)
+        {
+            rak=gec%10;
+            say[n]=rak;
+            gec=gec/10;
+            n++;
+        }
+        for(int j=0;j<n-1;j++)
+        {
+            if(say[j]<=say[j+1])
+                kont1++;
+            else if(say[j]>=say[j+1])
+                kont2++;
+        }
+        if(kont1!=n-1 && kont2!=n-1)
+               sicrayan++;
 
-                   if(i==top)
-                          {
-                              kont=1;
-                              break;}
-               }
-               if(kont==1)
-                break;
-
-         }
-          if(kont==0)
-            {
-                printf("\ni:%d",i);
-                break;
-            }
+        if(sicrayan*100/i==99)
+        {
+            printf("\nSicrayan:%d",i);
+            break;
+        }
     }
 }
-
-       return 0;
-}
-int asalMi(int i)
+int basamakSayi(int i)
 {
     int kont=0;
-    for(int x=2;x<=i/2;x++)
-    {
-        if(i%x==0)
+    while(i!=0)
+        {
             kont++;
-    }
-    if(kont==0)
-        return 0;
-    else
-        return 1;
-}
-int asalYolla(int j)
-{
-    int i,kont=0;
-    for(i=2;i<=j/2;i++)
-    {
-    if(j%i==0)
-            kont++;
-    }
-    if(kont==0)
-        return j;
-    if(j==2)
-        return j;
-    else
-        return 0;
+            i=i/10;
+        }
+        return kont;
 }
