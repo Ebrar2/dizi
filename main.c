d23d708abdb556df49c77b472e65b35ecf025b48
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 15:57:43 +0300

    Girilen elemanari tekrar sayisina gore siralama

--- main.c
+++ main.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
-#define M 5
-int diziYaz(int d[][M]);
+
 int main()
 {
-   int d[N][M],i,j;
-   printf("Dizinin elemanlarini giriniz:\n");
-   for(i=0;i<N;i++)
-   {
-       printf("%d.satiri girin:",i+1);
-       for(j=0;j<M;j++)
-         scanf("%d",&d[i][j]);
-     printf("\n");
-   }
-   printf("\nEnkucuk elemani:%d",diziYaz(d));
-    return 0;
-}
-int diziYaz(int d[][M])
-{
-    int i,j,enk=d[1][4];
-    printf("\nSag alt ucgen elemaanlari:");
-    for(i=0;i<N;i++)
+    int d[10],i,say=1,j,gec,gec2;
+    int a[10];
+    printf("Dizinin elemanlarini giriniz:\n");
+    for(i=0;i<10;i++)
+        scanf("%d",&d[i]);
+    for(i=0;i<10;i++)
     {
-        for(j=0;j<M;j++)
-        {
-            if((j+i)>=5)
-             {
-                printf("%3d",d[i][j]);
-                if(enk>d[i][j])
-                    enk=d[i][j];
-             }
-        }
+       say=1;
+       for(j=0;j<10;j++)
+       {
+           if(j!=i)
+           {
+           if(d[i]==d[j])
+              say++;
+           }
+       }
+       a[i]=say;
     }
-       return enk;
+    int b[10];
+    for(i=0;i<10;i++)
+        {
+        printf("%3d",a[i]);
+        b[i]=i;
+        }
+      for(i=0;i<10;i++)
+    {
+       for(j=0;j<9;j++)
+       {
+           if(a[j+1]<a[j])
+           {
+            gec=a[j];
+            a[j]=a[j+1];
+            a[j+1]=gec;
+            gec2=b[j+1];
+            b[j+1]=b[j];
+            b[j]=gec2;
+           }
+    }}
+    printf("\nDizinin siralanmis hali:\n");
+    for(i=0;i<10;i++)
+        printf("%3d",d[b[i]]);
+    return 0;
 }
