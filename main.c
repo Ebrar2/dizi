3e7b4af4e3943c9dcc0e1ae3508d06f1e738bf0b
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:13:21 +0300

    sutun toplamin bulma

--- main.c
+++ main.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-#define n 4
-#define m 4
-#define MAYINSAYISI 6
-void yerlestir(int tahta[][m]);
+
 int main()
 {
-    int tahta[n][m]={0};
-    int i,j;
-    yerlestir(tahta);
-    for(i=0;i<n;i++)
+    int d[4][6],i,j,elde=0,top=0;
+    for(i=0;i<4;i++)
     {
-        for(j=0;j<m;j++)
-            printf("%3d",tahta[i][j]);
-        printf("\n");
+        printf("\n%d.satir :",i);
+        for(j=0;j<6;j++)
+            scanf("%d",&d[i][j]);
+      printf("\n");
     }
-    return 0;
-}
-void yerlestir(int tahta[][m])
-{
-    int x,y,z;
-    srand(time(NULL));
-    for(x=0;x<MAYINSAYISI;x++)
+    printf("\n------------------------------\n");
+    int a[6];
+    for(i=0;i<4;i++)
     {
-        y=rand()%n;
-        z=rand()%m;
-       if(tahta[y][z]==0)
-        tahta[y][z]=1;
-       else
-        x--;
+        printf("\n%d.satir :",i);
+        for(j=0;j<6;j++)
+            printf("%3d",d[i][j]);
+      printf("\n");
     }
+    int x;
+    for(i=0;i<6;i++)
+    {
+        top=0;
+       for(j=0;j<4;j++)
+           top=top+d[j][5-i];
 
+    x=top%10+elde;
+    elde=top/10;
+    a[5-i]=x;
+    }
+    printf("\n      ");
+    for(i=0;i<6;i++)
+        printf("%3d",a[i]);
+    return 0;
 }
