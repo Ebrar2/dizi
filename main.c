a899a03e62cacf3da9b051dd65af559cf5c94c4e
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:07:36 +0300

    3x3 karenin kosegen elemanlari

--- main.c
+++ main.c
@@ -3,35 +3,31 @@
 
 int main()
 {
-    int d[5][4],i,j,top,enkE,enk=10000;
-    printf("Katmanlarim sertlik miktarlarini giriniz\n");
-    for(i=0;i<5;i++)
+    int a[3][3],i,j,top=0;
+
+    for(i=0;i<3;i++)
     {
-        printf("%d.katman(1.kuyudan 4.kuyuya dogru):",i+1);
-        for(j=0;j<4;j++)
-            scanf("%d",&d[i][j]);
-    printf("\n");
+        printf("%d.sira elemanlari:",i+1);
+        for(j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+      printf("\n");
     }
-    for(j=0;j<4;j++)
-    {
-        top=0;
-        for(i=0;i<5;i++)
-            top=top+d[i][j];
-    if(top<enk)
-    {
-        enk=top;
-        enkE=j;
-    }
-    }
-    top=0,i=0;
     int say=0;
-    printf("\nEnke:%d",enkE);
-    while(top<=10)
+    for(i=0;i<3;i++)
     {
-            top=top+d[i][enkE];
-            say++;
-            i++;
-            }
-    printf("\nBurgu %d.Kuyunun %d.Katmaninda degistirilir",enkE+1,say);
+        for(j=0;j<3;j++)
+        {    if(j+i==2)
+                top=top+a[i][j];
+            else if(i==j)
+            {
+            if(i==1 && j==1 && say<1)
+                {
+                    say++;
+                    top=top+a[i][j];
+                }
+            else
+                top=top+a[i][j];
+    }}}
+    printf("\nTop:%d",top);
     return 0;
 }
