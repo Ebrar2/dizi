849154b08d7db1512efae8c627f437d7b8fc56c4
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 15:59:10 +0300

    otoparkta park sirasini belirleme

--- main.c
+++ main.c
@@ -3,41 +3,47 @@
 
 int main()
 {
-   int d[4][3],i,j,x=0,gec;
-   int a[12];
-   printf("Dizinin sirasiz hali\n");
-   for(i=0;i<4;i++)
-   {
-       printf("%d.satiri giriniz:",i+1);
-       for(j=0;j<3;j++)
-         {scanf("%d",&d[i][j]);
-           a[x]=d[i][j];
-           x++;}
-    printf("\n");
-   }
-   for(i=1;i<12;i++)
-   {
-       for(j=0;j<11;j++)
-         if(a[j]>a[j+1])
+    int d[9][9]={0},i,j,x,say=0,e=0,num=1;
+    int a[9],kont=0;
+    while(say<9)
+    {
+      x=rand()%9;
+       if(say==0)
+         {
+        a[e]=x;
+        e++;
+        say++;
+    }
+       else
        {
-           gec=a[j+1];
-           a[j+1]=a[j];
-           a[j]=gec;
+           kont=0;
+           for(i=0;i<say;i++)
+           {
+               if(a[i]==x)
+                kont++;
+           }
+         if(kont==0)
+         {
+             a[e]=x;
+             e++;
+             say++;
+         }
        }
-   }
-   x=0;
-   for(i=0;i<4;i++)
-   {
-       for(j=0;j<3;j++)
-         {d[i][j]=a[x];
-         x++;
-   }}
-   printf("\nDizinin siralanmis hali:\n");
-   for(i=0;i<4;i++)
-   {
-       for(j=0;j<3;j++)
-         printf("%3d",d[i][j]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=9;j>-1;j--)
+        {
+            if(a[i]!=1 && a[i]!=4 &&  a[i]!=7)
+              {d[a[i]][a[j]]=num;
+              num++;
+    }}}
+     for(i=0;i<9;i++)
+    {
+        printf("%d.satir:",i);
+        for(j=0;j<9;j++)
+            printf("%3d",d[i][j]);
     printf("\n");
-   }
+    }
     return 0;
 }
