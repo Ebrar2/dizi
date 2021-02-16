70592b572161147878b27dc5083033014934adfe
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:03:59 +0300

    Satrancda 8 kalenin birbirini yeey

--- main.c
+++ main.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 3
-#define M 2
-void tekdenCifte(int c[][M],int d[],int e);
+#include<time.h>
 int main()
 {
-    int d[6]={1,2,3,4,5,6};
-    int c[N][M]={0};
-    tekdenCifte(c,d,6);
-    return 0;
-}
-
-void tekdenCifte(int c[][M],int d[],int e)
-{
-    int i,j,a=0;
-    for(i=0;i<N;i++)
+    char d[8][8];
+    int i,j,sut[8],say=0;
+    for(i=0;i<8;i++)
     {
-        for(j=0;j<M;j++)
-            c[i][j]=d[a];
-       a++;
+        for(j=0;j<8;j++)
+            d[i][j]='-';
     }
-    printf("\n");
-    for(i=0;i<N;i++)
+    srand(time(NULL));
+    while(say<8)
     {
-        for(j=0;j<M;j++)
-            printf("%d",c[i][j]);
+        if(say==0)
+            {sut[say]=rand()%8;
+            say++;
+}
+      else
+      {
+          int kont=0;
+          j=rand()%8;
+          for(i=0;i<say;i++)
+             {
+                if(j==sut[i])
+                    kont++;}
+        if(kont==0)
+           {
+            sut[say]=j;
+            say++;}
+      }}
+     for(i=0;i<8;i++)
+         d[i][sut[i]]='K';
+    printf("\n ");
+      for(i=0;i<8;i++)
+        printf("%d",i);
+    printf("\n");
+    for(i=0;i<8;i++)
+    {
+          printf("%d",i);
+        for(j=0;j<8;j++)
+            printf("%c",d[i][j]);
       printf("\n");
     }
+
+    return 0;
 }
