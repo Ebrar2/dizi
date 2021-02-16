9b407b65a225e9f08ed2c641366646200b0ebf9c
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:07:02 +0300

    sondaj vurulmasi icin uygun yerler

--- main.c
+++ main.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+
 int main()
 {
-    char d[8][8];
-    int i,j,sut[8],say=0;
-    for(i=0;i<8;i++)
+    int d[5][4],i,j,top,enkE,enk=10000;
+    printf("Katmanlarim sertlik miktarlarini giriniz\n");
+    for(i=0;i<5;i++)
     {
-        for(j=0;j<8;j++)
-            d[i][j]='-';
-    }
-    srand(time(NULL));
-    while(say<8)
-    {
-        if(say==0)
-            {sut[say]=rand()%8;
-            say++;
-}
-      else
-      {
-          int kont=0;
-          j=rand()%8;
-          for(i=0;i<say;i++)
-             {
-                if(j==sut[i])
-                    kont++;}
-        if(kont==0)
-           {
-            sut[say]=j;
-            say++;}
-      }}
-     for(i=0;i<8;i++)
-         d[i][sut[i]]='K';
-    printf("\n ");
-      for(i=0;i<8;i++)
-        printf("%d",i);
+        printf("%d.katman(1.kuyudan 4.kuyuya dogru):",i+1);
+        for(j=0;j<4;j++)
+            scanf("%d",&d[i][j]);
     printf("\n");
-    for(i=0;i<8;i++)
-    {
-          printf("%d",i);
-        for(j=0;j<8;j++)
-            printf("%c",d[i][j]);
-      printf("\n");
     }
-
+    for(j=0;j<4;j++)
+    {
+        top=0;
+        for(i=0;i<5;i++)
+            top=top+d[i][j];
+    if(top<enk)
+    {
+        enk=top;
+        enkE=j;
+    }
+    }
+    top=0,i=0;
+    int say=0;
+    printf("\nEnke:%d",enkE);
+    while(top<=10)
+    {
+            top=top+d[i][enkE];
+            say++;
+            i++;
+            }
+    printf("\nBurgu %d.Kuyunun %d.Katmaninda degistirilir",enkE+1,say);
     return 0;
 }
