8419417d59639b6eb4b2aaf94c62af2cc2b5a896
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:17:40 +0300

    ilk 100 sayinin karesinin top ve il 100 toplaminin karesi

--- main.c
+++ main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-int asalMi(int j);
+
 int main()
 {
-    int d[10002],i,j;
-    d[0]=2;
-    i=1;
-    for(j=3;;j++)
+    int i,top=0,kareTop=0;
+    for(i=1;i<=100;i++)
     {
-       if(asalMi(j)==0)
-                { d[i]=j;
-                i++;}
-        if(i==10002)
-        {
-              printf("Ara:%d",d[10002]);
-              break;
-        }
+        top=top+i;
+        kareTop=kareTop+i*i;
     }
-
+    printf("Fark:%d",top*top-kareTop);
     return 0;
-}
-int asalMi(int j)
-{
-    int kont=0,i;
-    for(i=2;i<j/2;i++)
-    {
-        if(j%i==0)
-            kont++;
-    }
-    if(kont==0)
-        return 0;
-    else
-        return 1;
 }
