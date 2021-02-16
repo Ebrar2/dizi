230241d3f94cd2072aa7f23aa4225ae42ec8e0c5
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:00:53 +0300

    2 boyutlu diziyi 180 derece bolme

--- main.c
+++ main.c
@@ -1,47 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+
 int main()
 {
-    int d[7][7]={0},i,j,x,e=0,say=0,kont=0;
-    int a[7],sira=1;
-    srand(time(NULL));
-    while(say<7)
+    int a[3][3],i,j;
+    printf("Dizinin Elemanlari\n");
+    for(i=0;i<3;i++)
     {
-        if(say==0)
-            {
-                a[e]=rand()%8;
-                say++;
-                e++;}
-        else
-        {
-            kont=0;
-            x=rand()%8;
-            for(i=0;i<e;i++)
-              {
-                  if(a[i]==x)
-                    kont++;
-              }
-            if(kont==0)
-                {
-                     a[e]=x;
-                     e++;
-                     say++;
-                }
-        }
+        printf("\na dizisinin %d.satiri:",i+1);
+        for(j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
     }
-    for(i=0;i<7;i++)
+    printf("\n-------------------------------------------------\n");
+    for(j=0;j<3;j++)
     {
-        for(j=6;j>-1;j--)
-          {
-           d[a[i]][a[j]]=sira;
-           sira++;}
-    }
-    for(i=0;i<7;i++)
-    {
-        for(j=0;j<7;j++)
-            printf("%3d",d[i][j]);
-     printf("\n");
+        for(i=0;i<3;i++)
+          printf("%3d",a[i][j]);
+      printf("\n");
     }
     return 0;
 }
