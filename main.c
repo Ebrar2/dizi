e214bfde35f81317c89c1068dd61266dacb87ed7
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:21:37 +0300

    1000 alti bilesik sayi

--- main.c
+++ main.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<math.h>
+int asalYolla(int j);
+int asalMi(int i);
 
 int main()
 {
-    int i,top=0;
-    for(i=0;i<1000;i++)
+    int i,top,j,a,kont;
+    for(i=36;i<6000;i++)
     {
-        if(i%3==0 || i%5==0)
-            top=top+i;
+        kont=0;
+      if(i%2!=0 && asalMi(i)==1)
+         {
+           for(a=1;a<sqrt(i/2);a++)
+           {
+               for(j=2;j<=i-2;j++)
+               {
+                   if(asalYolla(j)==j)
+                        top=j+2*a*a;
+
+                   if(i==top)
+                          {
+                              kont=1;
+                              break;}
+               }
+               if(kont==1)
+                break;
+
+         }
+          if(kont==0)
+            {
+                printf("\ni:%d",i);
+                break;
+            }
     }
-    printf("Top:%d",top);
-    return 0;
+}
+
+       return 0;
+}
+int asalMi(int i)
+{
+    int kont=0;
+    for(int x=2;x<=i/2;x++)
+    {
+        if(i%x==0)
+            kont++;
+    }
+    if(kont==0)
+        return 0;
+    else
+        return 1;
+}
+int asalYolla(int j)
+{
+    int i,kont=0;
+    for(i=2;i<=j/2;i++)
+    {
+    if(j%i==0)
+            kont++;
+    }
+    if(kont==0)
+        return j;
+    if(j==2)
+        return j;
+    else
+        return 0;
 }
