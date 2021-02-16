f6ec688a783c10a86b191fcaead4717a4f5067c9
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:20:20 +0300

    fibonacci cift degerleri top

--- main.c
+++ main.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-int asalMi(int i);
+
 int main()
 {
-    int i,kont;
-    for(i=3;i<600851475143/2;i++)
+    long int top=0,d[50000];
+    int i;
+    d[0]=1;
+    d[1]=1;
+    for(i=2; ;i++)
     {
-        if(asalMi(i)==0)
-        {
-            if(600851475143%i==0)
-             {
-                kont=i;
-               printf("\nkont:%d",kont);}
-
-        }}
-
-    printf("\nAranan:%d",kont);
-    return 0;
-}
-int asalMi(int i)
-{
-    int kont=0,x;
-    for(x=2;x<=i/2;x++)
-    {
-        if(i%x==0)
-            kont++;
+        d[i]=d[i-1]+d[i-2];
+        if(d[i]%2==0)
+            top=top+d[i];
+        if(d[i]>4000000)
+            break;
     }
-    if(kont==0)
-        return 0;
-    else
-        return 1;
+    printf("TOP:%d",top);
+
+    return 0;
 }
