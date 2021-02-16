35bc1a3940d0450e643b658bfeec2b19482c8127
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 16:15:26 +0300

    ozel pisagor uclusu

--- main.c
+++ main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
 int main()
 {
-   unsigned long int i,top=17,x,kont=0;
-   for(i=11;i<2000000;i+=2)
-   {
-        for(x=2;x<=i/2;x++)
-  {
-      if(i%x==0)
-       {
-        kont++;
-        break;}
-  }
-   if(kont==0)
-        top=top+i;
-   kont=0;
-  if(i>1000000)
-     printf("top:%ld",top);
-   }
-
+    long int i,j,z,top;
+    for(i=150; ;i++)
+    {
+        for(z=100;z<i;z++)
+        {
+            for(j=100;j<i;j++)
+            {
+                top=i+j+z;
+                if(top==1000 && j*j+z*z==i*i)
+                {
+                    printf("\nOzel uclu carp:%ld",i*z*j);
+                    break;
+                     }
+            }
+        }
+    }
     return 0;
 }
