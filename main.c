96304a4304344c04be1ebd62a6f1e3e47dc5976b
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 15:49:30 +0300

    Robota verilen komutlariyla hareket ettigi yerleri 1 digerlerine 0 atama

--- main.c
+++ main.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 3
-#define M 2
-void tekdenCifte(int c[][M],int d[],int e);
+
 int main()
 {
-    int d[6]={1,2,3,4,5,6};
-    int c[N][M]={0};
-    tekdenCifte(c,d,6);
+    int d[6][5]={0},x,y,i=0,j;
+    printf("Robotu konumunu giriniz:");
+    scanf("%d%d",&x,&y);
+    d[x][y]=1;
+    printf("\n-----------------------------------------------------\n");
+    printf("(1:sag,2:sol,3:asagi,4:yukari");
+    printf("\nKomutlarinizi giriniz:");
+    do
+    {
+        scanf("%d",&i);
+        if(i==1)
+        {
+            if(y+1<5)
+                {d[x][y+1]=1;
+                y++;}
+            }
+        else if(i==2)
+        {
+            if(y-1>-1)
+            {
+            d[x][y-1]=1;
+             y--;
+            }}
+        else if(i==3)
+        {
+            if(x+1<6)
+              {d[x+1][y]=1;
+               x++;
+        }}
+        else if(i==4)
+        {
+            if(x-1>-1)
+            {   d[x-1][y]=1;
+                x--;
+        }}
+    }while(i!=0);
+    printf("\n------------------------------------------------------------\n");
+    printf("\nRobotu rotasi:\n");
+    for(i=0;i<6;i++)
+    {
+        printf("%d.sat:",i);
+         for(j=0;j<5;j++)
+            printf("%3d",d[i][j]);
+       printf("\n");
+    }
     return 0;
-}
-
-void tekdenCifte(int c[][M],int d[],int e)
-{
-    int i,j,a=0;
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<M;j++)
-            c[i][j]=d[a];
-       a++;
-    }
-    printf("\n");
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<M;j++)
-            printf("%d",c[i][j]);
-      printf("\n");
-    }
 }
