55946c2b3ea9453adf914d110388af9e4b79b076
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-16 15:57:00 +0300

    Kare matrisde ust ucgensel matrisdeki en kucuk eleman

--- main.c
+++ main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 5
+#define M 5
+int diziYaz(int d[][M]);
 int main()
 {
-    int d[5][3],i,j,top,say=0,a[5];
-    for(i=0;i<5;i++)
-    {
-        top=0;
-        printf("%d.ogrencinin notlari:",i+1);
-         for(j=0;j<3;j++)
-            {scanf("%d",&d[i][j]);
-              top=top+d[i][j];}
-         a[i]=top;
-        printf("\n");
-    }
-    printf("\nKalinan dersler:\n");
-    for(i=0;i<5;i++)
-    {
-        printf("%d.ogrencinin kaldigi ders sayisi:",i+1);
-        say=0;
-         for(j=0;j<3;j++)
-         {
-             if(d[i][j]<=40)
-               say++;
-             else if(d[i][j]>40 && d[i][j]<60)
-             {
-                 if(a[i]/3<60)
-                    say++;
-             }
-         }
-        printf("%d\n",say);
-    }
-
-
+   int d[N][M],i,j;
+   printf("Dizinin elemanlarini giriniz:\n");
+   for(i=0;i<N;i++)
+   {
+       printf("%d.satiri girin:",i+1);
+       for(j=0;j<M;j++)
+         scanf("%d",&d[i][j]);
+     printf("\n");
+   }
+   printf("\nEnkucuk elemani:%d",diziYaz(d));
     return 0;
+}
+int diziYaz(int d[][M])
+{
+    int i,j,enk=d[1][4];
+    printf("\nSag alt ucgen elemaanlari:");
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<M;j++)
+        {
+            if((j+i)>=5)
+             {
+                printf("%3d",d[i][j]);
+                if(enk>d[i][j])
+                    enk=d[i][j];
+             }
+        }
+    }
+       return enk;
 }
