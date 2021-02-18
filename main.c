14e4a13f1c81a752b2ffa9386ad556540729ad21
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-18 21:50:34 +0300

    pascal ucgenni yazdirma

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
+int kombinasyon(int n,int j);
+int fakt(int n);
 int main()
 {
-    int say,i=0,k=0;
-    printf("Hangi sayinin logaritmasini bulmak istiyorsun:");
-    scanf("%d",&say);
-    while(say>pow(2,i))
-    {
-        if(say==pow(2,i))
-            k=pow(2,i);
-       else if(say>pow(2,i))
-        {
-            while(say>=pow(2,k))
-                k++;
-        }
-        i++;
-    }
-    printf("\nLog cevabi:%d",k-1);
+   int d[200],d2[200],i,j,k,e=0,a=0;
+   for(i=0;i<10;i++)
+     d2[i]=i+1;
+    for(k=0;k<13;k++)
+        printf("  ");
+    printf("1");
+    printf("\n");
+   for(i=0;i<10;i++)
+   {
+       a=0;
+       for(k=i;k<10;k++)
+          printf("  ");
+       for(j=0;j<=d2[e];j++)
+       {
+           printf("%5d",kombinasyon(d2[e],a));
+           a++;
+       }
+           e++;
+       printf("\n");
+   }
     return 0;
 }
+int kombinasyon(int n,int j)
+{
+    int kom;
+    kom=fakt(n)/(fakt(n-j)*fakt(j));
+        return kom;
+}
+int fakt(int n)
+{   int sonuc=1;
+    if(n==0 || n==1)
+        return 1;
+    else
+      {while(n!=1)
+      {
+        sonuc=sonuc*n;
+        n--;
+      }}
+    return sonuc;
+  }
