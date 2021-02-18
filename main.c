5d8ed9bfe0acaaa0720bb7d3250a07c899f5a5ec
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-02-18 21:48:40 +0300

    109 a bolunen palindrom sayilar

--- main.c
+++ main.c
@@ -3,12 +3,43 @@
 #include<math.h>
 int main()
 {
-    int i,a=0;
-    for(i=1;i<10;i++)
-    {
-        a=a+pow(i,i);
-        printf("\nSon basamakdaki sayi:%d",a%10);
-    }
-
+    int x,i;
+    int basamak=0,bas=0,son=0,gec=0,kont=0,say=0;
+    for(i=110;i<100000;i++)
+        {
+         gec=i;
+         bas=i;
+         while(gec!=0)
+         {
+             basamak++;
+             x=gec%10;
+             gec=gec/10;
+         }
+         while(kont<basamak/2)
+         {
+             x=bas%10;
+             son=son*10+x;
+             bas=bas/10;
+             kont++;
+         }
+         if(basamak%2==0)
+         {
+             if(bas==son)
+              {if(i%109==0)
+                say++;
+         }}
+         else
+         {
+             if(son==(bas/10))
+             {
+                 if(i%109==0)
+                 say++;
+             }
+         }
+          kont=0;
+         basamak=0;
+         son=0;
+        }
+        printf("%d tane",say);
     return 0;
 }
